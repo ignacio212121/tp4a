54a5fd7b845414e515e019da713c6264cf0f6f7c
Marco Marino <marcomarinoj@gmail.com>
2024-11-04 20:59:22 -0300

    cambios

--- node_2d.gd
+++ node_2d.gd
@@ -1,36 +1,64 @@
 extends Node2D
 @onready var viewport = $SubViewport
-@onready var brush = viewport.get_node("CanvasLayer/Brush") # Este es el pincel
-@onready var image_top = $Arriba # La imagen superior que queremos hacer transparente
+@onready var brush = viewport.get_node("CanvasLayer/Brush")
+@onready var image_top = $Arriba
+
+var dir_resultados = "res://resultados.dat"
+
+var resultados = {
+	"totales": 0,
+	"opcion_1": 0,
+}
 
 var accumulated_texture
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	$SubViewport.size = Vector2(1920, 1080)
-	# Crear una imagen para la textura acumulada que coincida con el tamaño del Viewport
 	accumulated_texture = ImageTexture.new()
 	accumulated_texture.resource_local_to_scene = true
 
 	accumulated_texture.set_image(viewport.get_texture().get_image())
-	# Asignar la textura acumulada al shader de la imagen superior
 	image_top.material.set_shader_parameter("mask_texture", accumulated_texture)
+	_guardar(resultados, dir_resultados)
 
 func _process(delta):
-	# Mover el pincel para que siga la posición del cursor dentro del Viewport
 	$Cursor.position.x = get_local_mouse_position().x 
 	$Cursor.position.y = get_local_mouse_position().y 
 	brush.position = get_local_mouse_position()
-	# Dibujar solo si el clic izquierdo está presionado
 	if Input.is_action_pressed("mouse_left"):
 		brush.visible = true
 		_draw_to_accumulated_texture()
 	else:
 		brush.visible = false
-
+	if Input.is_action_just_pressed("ui_right"):
+		resultados["opcion_1"] += 1
+		resultados["totales"] += 1
+		_guardar(resultados, dir_resultados)
+	if Input.is_action_just_pressed("ui_left"):
+		resultados["totales"] += 1
+		_guardar(resultados, dir_resultados)
+	if Input.is_action_just_pressed("ui_accept"):
+		if resultados["totales"] != 0:
+			print(resultados["opcion_1"])
+			print(resultados["totales"])
+			print("Opcion 1: ",resultados["opcion_1"] / resultados["totales"] * 100, "%")
+			print("Opcion 2: ", (resultados["totales"] - resultados["opcion_1"]) / resultados["totales"] * 100, "%")
+		else:
+			print("Opcion 1: 0%")
+			print("Opcion 2: 0%")
 func _draw_to_accumulated_texture():
 	
-	# Captura el contenido del Viewport en una imagen y acumúlalo en la textura
 	var img = viewport.get_texture().get_image()
 	accumulated_texture.update(img)
 	$SubViewport/CanvasLayer/TextureRect2.texture = accumulated_texture
 	
+func _guardar(datos, dir):
+	var file = FileAccess.open(dir, FileAccess.WRITE)
+	file.store_var(datos)
+	file = null
+	
+func _cargar(dir):
+	var file = FileAccess.open(dir, FileAccess.READ)
+	var data = file.get_var()
+	file = null
+	return data
